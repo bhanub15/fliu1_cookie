c0902a866ba8ea46925074e16e83de4a05ddf776
bbandaru <bbandaru@starbucks.com>
2023-09-03 22:52:33 -0700

    1.20

--- WebApp_Security/WebApp_UnderTheHood/Pages/HRManager.cshtml.cs
+++ WebApp_Security/WebApp_UnderTheHood/Pages/HRManager.cshtml.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using WebApp_UnderTheHood.Authorization;
 using WebApp_UnderTheHood.DTO;
+using WebApp_UnderTheHood.Pages.Account;
 
 namespace WebApp_UnderTheHood.Pages
 {
@@ -27,6 +31,13 @@ namespace WebApp_UnderTheHood.Pages
         public async Task OnGetAsync()
         {
             var httpClient = httpClientFactory.CreateClient("OurWebAPI");
+            var res = await httpClient.PostAsJsonAsync("auth", new Credential { UserName = "admin", Password = "password" });
+            res.EnsureSuccessStatusCode();
+            string strJwt = await res.Content.ReadAsStringAsync();
+            var token = JsonConvert.DeserializeObject<JwtToken>(strJwt);
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+
             WeatherForecastItems = await httpClient.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast");
         }
     }
