3c7e1cb6df3259942b5d88affa1579b3c8b02f83
bbandaru <bbandaru@starbucks.com>
2023-09-03 22:43:41 -0700

    1.10

--- WebApp_Security/WebApp_UnderTheHood/Startup.cs
+++ WebApp_Security/WebApp_UnderTheHood/Startup.cs
@@ -26,6 +26,22 @@ namespace WebApp_UnderTheHood
             services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", options =>
             {
                 options.Cookie.Name = "MyCookieAuth";
+                options.LoginPath = "/Account/Login";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+            });
+
+            services.AddAuthorization(options => {
+
+                options.AddPolicy("AdminOnly",
+                    policy => policy.RequireClaim("Admin"));
+
+                options.AddPolicy("MustBelongToHRDepartment",
+                    policy => policy.RequireClaim("Department", "HR"));
+
+                options.AddPolicy("HRManagerOnly", policy => policy
+                    .RequireClaim("Department", "HR")
+                    .RequireClaim("Manager"));
+
             });
 
             services.AddRazorPages();
